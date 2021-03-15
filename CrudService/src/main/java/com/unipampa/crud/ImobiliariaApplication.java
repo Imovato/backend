22b3fc4c5766044aed516db7993558ac178ec6ea
Lucas Fell <47724710+fell-lucas@users.noreply.github.com>
2021-03-15 14:50:34 -0300

    moved cors to config

--- CrudService/src/main/java/com/unipampa/crud/ImobiliariaApplication.java
+++ CrudService/src/main/java/com/unipampa/crud/ImobiliariaApplication.java
@@ -3,10 +3,6 @@ package com.unipampa.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -16,15 +12,4 @@ public class ImobiliariaApplication {
 		SpringApplication.run(ImobiliariaApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/api/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("PUT", "GET", "POST", "DELETE");
-				}    
-		};
-	}
 }
