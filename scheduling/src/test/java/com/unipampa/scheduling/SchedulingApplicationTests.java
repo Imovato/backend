01763e282d03182df713dc2093a8dd0bd67f4e63
Auri Gabriel Castro de Melo <12087669+auri-gabriel@users.noreply.github.com>
2021-03-22 15:51:59 -0300

    Create Scheduling MicroService
    Started a new MicroService

---
+++ scheduling/src/test/java/com/unipampa/scheduling/SchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipampa.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
