bb04bea937e1b2d9c3beac8cbc93e3235f184d08
Auri Gabriel Castro de Melo <12087669+auri-gabriel@users.noreply.github.com>
2021-03-30 10:23:45 -0300

    Added RabbitMQ integration

---
+++ scheduling/src/main/java/com/unipampa/scheduling/message/PropertyReceiveMessage.java
@@ -0,0 +1,25 @@
+package com.unipampa.scheduling.message;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.unipampa.scheduling.model.Property;
+import com.unipampa.scheduling.repository.PropertyRepository;
+
+@Component
+public class PropertyReceiveMessage {
+	
+	private PropertyRepository propertyRepository;
+	
+	@Autowired
+	public PropertyReceiveMessage(PropertyRepository propertyRepository) {
+		this.propertyRepository = propertyRepository;
+	}
+	
+	@RabbitListener(queues = {"${crud.rabbitmq.queue}"})
+	public void receive(@Payload Property property) {
+		propertyRepository.save(property);
+	}
+}
