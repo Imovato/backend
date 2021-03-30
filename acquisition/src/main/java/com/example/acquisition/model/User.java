e2dc122401d4dcefb72980eeff9ac78a5c134ead
Samuel Modesto <samuelmodestoes@gmail.com>
2021-03-30 12:34:30 -0300

    microservice acquisition with RabbitMQ

---
+++ acquisition/src/main/java/com/example/acquisition/model/User.java
@@ -0,0 +1,55 @@
+package com.example.acquisition.model;
+
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@OneToMany
+	private List<Property> properties;
+
+	public User(String name) {
+		this.name = name;
+	}
+
+	public User() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Property> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(List<Property> properties) {
+		this.properties = properties;
+	}
+
+}
