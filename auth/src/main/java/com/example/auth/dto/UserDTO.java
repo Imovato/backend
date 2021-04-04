62f52019bcc32b9b3fd1fe98f56d32155d12af14
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-04 16:45:21 -0300

    separated UserDTO and UserDetails (from security) in MyUserDetails, added some constraints

--- auth/src/main/java/com/example/auth/dto/UserDTO.java
+++ auth/src/main/java/com/example/auth/dto/UserDTO.java
@@ -1,25 +1,36 @@
 package com.example.auth.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.List;
+
+import com.example.auth.model.Role;
 public class UserDTO {
 
-	private String userName;
-	private String password;
+	@ApiModelProperty(position = 0)
+  private String username;
+  @ApiModelProperty(position = 1)
+  private String email;
+  @ApiModelProperty(position = 2)
+  private String password;
+  @ApiModelProperty(position = 3)
+  List<Role> roles;
 
-	public UserDTO() {
-	}
+	public String getUsername() {
+    return username;
+  }
 
-	public UserDTO(String userName, String password) {
-		this.userName = userName;
-		this.password = password;
-	}
+  public void setUsername(String username) {
+    this.username = username;
+  }
+	
+	public String getEmail() {
+    return email;
+  }
 
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
 	public String getPassword() {
 		return password;
@@ -28,4 +39,12 @@ public class UserDTO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public List<Role> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(List<Role> roles) {
+    this.roles = roles;
+  }
 }
--- auth/src/main/java/com/example/auth/model/User.java
+++ auth/src/main/java/com/example/auth/model/User.java
@@ -1,126 +1,75 @@
 package com.example.auth.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
-import org.springframework.security.access.method.P;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-@Table(name = "users")
-public class User implements UserDetails {
+public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Integer id;
 
-	@Column(name = "user_name", unique = true)
-	private String userName;
+	@Size(min = 4, max = 255, message = "Tamanho mínimo do nome: 4 caracteres")
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
 
-	@Column(name = "password")
+	@Size(min = 8, message = "Tamanho mínimo da senha: 8 caracteres")
 	private String password;
 
-	@Column(name = "accontNonExpired")
-	private Boolean accontNonExpired;
-
-	@Column(name = "accontNonLocked")
-	private Boolean accontNonLocked;
-
-	@Column(name = "credentialsNonExpired")
-	private Boolean credentialsNonExpired;
-
-	@Column(name = "enabled")
-	private Boolean enabled;
-
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "user_permission", joinColumns = { @JoinColumn(name = "id_user") },
-	inverseJoinColumns = {@JoinColumn(name = "id_permissions") })
-
-	private List<Permission> permissions;
-
-	public User() {
-
-	}
-
-	public User(Long id, String userName, String password) {
-		this.id = id;
-		this.userName = userName;
-		this.password = password;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.permissions;
-	}
+	@ElementCollection(fetch = FetchType.EAGER)
+  List<Role> roles;
 	
-	public List<String> getRoles(){
-		List<String> roles = new ArrayList<>();
-		this.permissions.stream().forEach(p -> {
-			roles.add(p.getDescription());
-		});
-		return null;
+	public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+	public List<Role> getRoles(){
+		return roles;
 	}
 
-	@Override
-	public String getUsername() {
-		return this.userName;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return this.isAccountNonExpired();
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return this.isAccountNonLocked();
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return this.isCredentialsNonExpired();
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return this.enabled;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	public void setRoles(List<Role> roles) {
+    this.roles = roles;
+  }
 
 }
