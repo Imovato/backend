c180cefd3845517311cc93b1993dfd55f821969e
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-04 01:50:52 -0300

    more reliable image upload

--- CrudService/src/main/java/com/unipampa/crud/utils/FileUploadUtil.java
+++ CrudService/src/main/java/com/unipampa/crud/utils/FileUploadUtil.java
@@ -6,16 +6,16 @@ import java.nio.file.*;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
-  public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
+  public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) {
     Path uploadPath = Paths.get(uploadDir);
 
-    Files.createDirectories(uploadPath);
-
     try (InputStream inputStream = multipartFile.getInputStream()) {
+      Files.createDirectories(uploadPath);
       Path filePath = uploadPath.resolve(fileName);
       Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
     } catch (IOException ioe) {
-      throw new IOException("Could not save image file: " + fileName, ioe);
+      System.out.println("Could not save image file.");
+      ioe.printStackTrace();
     }
   }
 }
