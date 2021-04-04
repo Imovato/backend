5e452eef8752b29b8484b64d871842864c7b371c
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-04 17:12:25 -0300

    signup, delete, search, whoami and refresh token, also fixed user import

--- auth/src/main/java/com/example/auth/service/UserService.java
+++ auth/src/main/java/com/example/auth/service/UserService.java
@@ -5,9 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.example.auth.exception.CustomHttpException;
+import com.example.auth.model.User;
 import com.example.auth.repository.UserRepository;
 import com.example.auth.security.JwtTokenProvider;
 
@@ -16,6 +20,9 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
@@ -30,6 +37,36 @@ public class UserService {
 		} catch(AuthenticationException e) {
 			throw new CustomHttpException("Combinação de usuário/senha inválida", HttpStatus.UNPROCESSABLE_ENTITY);
 		}
+	}
+
+	public String signup(User user) {
+		if (!userRepository.existsByUsername(user.getUsername())) {
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			userRepository.save(user);
+			return jwtTokenProvider.createToken(user.getUsername(), user.getRoles());
+		} else {
+			throw new CustomHttpException("Nome de usuário já existe", HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+	}
+
+	public void delete(String username) {
+		userRepository.deleteByUsername(username);
+	}
+
+	public User search(String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			throw new CustomHttpException("O usuário não existe", HttpStatus.NOT_FOUND);
+		}
+		return user;
+	}
+
+	public User whoami(HttpServletRequest req) {
+		return userRepository.findByUsername(jwtTokenProvider.getUsername(jwtTokenProvider.resolveToken(req)));
+	}
+
+	public String refresh(String username) {
+		return jwtTokenProvider.createToken(username, userRepository.findByUsername(username).getRoles());
 	}
 
 }
