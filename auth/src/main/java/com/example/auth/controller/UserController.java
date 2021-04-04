d897945751fb67502365eb5705ebdb7d0ea0064b
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-04 17:41:13 -0300

    user controller routes

--- auth/src/main/java/com/example/auth/controller/UserController.java
+++ auth/src/main/java/com/example/auth/controller/UserController.java
@@ -1,6 +1,21 @@
 package com.example.auth.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.example.auth.dto.UserDTO;
+import com.example.auth.model.User;
+import com.example.auth.service.UserService;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -9,5 +24,44 @@ import io.swagger.annotations.Api;
 @RequestMapping("/users")
 @Api(tags = "users")
 public class UserController {
+  
+  @Autowired
+  private UserService userService;
 
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @PostMapping("/signin")
+  public String login(@RequestParam String username, @RequestParam String password) {
+    return userService.signin(username, password);
+  }
+
+  @PostMapping("/signup")
+  public String signup(@RequestBody UserDTO user) {
+    return userService.signup(modelMapper.map(user, User.class));
+  }
+
+  @DeleteMapping(value = "/{username}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public String delete(@PathVariable String username) {
+    userService.delete(username);
+    return username;
+  }
+
+  @GetMapping(value = "/{username}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public UserDTO search(@PathVariable String username) {
+    return modelMapper.map(userService.search(username), UserDTO.class);
+  }
+
+  @GetMapping(value = "/me")
+  public UserDTO whoami(HttpServletRequest req) {
+    return modelMapper.map(userService.whoami(req), UserDTO.class);
+  }
+
+  @GetMapping("/refresh")
+  @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CLIENT')")
+  public String refresh(HttpServletRequest req) {
+    return userService.refresh(req.getRemoteUser());
+  }
 }
