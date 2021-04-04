59166d91ab08c12b8689e48d48deac42593358f0
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-04 17:31:51 -0300

    user controller

---
+++ auth/src/main/java/com/example/auth/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping("/users")
+@Api(tags = "users")
+public class UserController {
+
+}
