604763a56adda75902dea78c35ccd7dfc7dd616a
Auri Gabriel Castro de Melo <12087669+auri-gabriel@users.noreply.github.com>
2021-04-06 14:35:56 -0300

    Rearrangement of the code for better readability

--- CrudService/src/main/java/com/unipampa/crud/controller/PropertyController.java
+++ CrudService/src/main/java/com/unipampa/crud/controller/PropertyController.java
@@ -43,141 +43,9 @@ public class PropertyController {
 		this.propertyService = service;
 	}
 
-	@GetMapping("property/find/{id}")
-	@ApiOperation(value = "Encontra uma propriedade através do id")
-	public ResponseEntity<?> getPropertyById(@PathVariable("id") Long id) {
-		Property property = propertyService.getPropertyById(id);
-		return new ResponseEntity<>(property, HttpStatus.OK);
-	}
-
-	@GetMapping("apartment/all")
-	@ApiOperation(value = "Retorna uma lista de apartamentos")
-	public ResponseEntity<?> getAllApartments() {
-		List<Property> properties = propertyService.findAllByDtype("Apartment");
-		return new ResponseEntity<>(properties, HttpStatus.OK);
-	}
-
-	@GetMapping("house/all")
-	@ApiOperation(value = "Retorna uma lista de casas")
-	public ResponseEntity<?> getAllHouses() {
-		List<Property> properties = propertyService.findAllByDtype("House");
-		return new ResponseEntity<>(properties, HttpStatus.OK);
-	}
-
-	@GetMapping("ground/all")
-	@ApiOperation(value = "Retorna uma lista de terrenos")
-	public ResponseEntity<?> getAllGrounds() {
-		List<Property> properties = propertyService.findAllByDtype("Ground");
-		return new ResponseEntity<>(properties, HttpStatus.OK);
-	}
-
-	@PostMapping("/house")
-	@ApiOperation(value = "Salva uma casa ")
-	public ResponseEntity<?> saveHouse(@RequestBody PropertyDTO houseDto) {
-		House house = new House();
-		house.setArea(houseDto.getArea());
-		house.setName(houseDto.getName());
-		house.setNeighborhood(houseDto.getNeighborhood());
-		house.setCodAddress(houseDto.getCodAddress());
-		house.setCity(houseDto.getCity());
-		house.setDescription(houseDto.getDescription());
-		house.setAdress(houseDto.getAdress());
-		house.setState(houseDto.getState());
-		house.setPrice(houseDto.getPrice());
-		house.setNumber(houseDto.getNumber());
-		house.setRooms(houseDto.getRooms());
-		house.setAmount(houseDto.getAmount());
-		propertyService.saveProperty(house);
-		return new ResponseEntity<>(house, HttpStatus.OK);
-	}
-
-	@GetMapping("house/find/{id}")
-	@ApiOperation(value = "Encontra uma casa através do ID")
-	public ResponseEntity<?> getHouseById(@PathVariable("id") Long id) {
-		House house = propertyService.getHouseById(id);
-		return new ResponseEntity<>(house, HttpStatus.OK);
-	}
-
-	@PutMapping("/update/house")
-	@ApiOperation(value = "Atualiza as informações de uma casa")
-	public ResponseEntity<?> updateHouse(@RequestBody House house) {
-		House updatedHouse = propertyService.updateHouse(house);
-		return new ResponseEntity<>(updatedHouse, HttpStatus.OK);
-	}
-
-	@PostMapping("/apartment")
-	@ApiOperation(value = "Salva um apartamento")
-	public ResponseEntity<?> saveApartment(@RequestBody PropertyDTO apartmentDto) {
-		Apartment apartment = new Apartment();
-		apartment.setArea(apartmentDto.getArea());
-		apartment.setName(apartmentDto.getName());
-		apartment.setNeighborhood(apartmentDto.getNeighborhood());
-		apartment.setCodAddress(apartmentDto.getCodAddress());
-		apartment.setCity(apartmentDto.getCity());
-		apartment.setDescription(apartmentDto.getDescription());
-		apartment.setAdress(apartmentDto.getAdress());
-		apartment.setState(apartmentDto.getState());
-		apartment.setPrice(apartmentDto.getPrice());
-		apartment.setNumber(apartmentDto.getNumber());
-		apartment.setBlock(apartmentDto.getBlock());
-		apartment.setRooms(apartmentDto.getRooms());
-		apartment.setAmount(apartmentDto.getAmount());
-		propertyService.saveProperty(apartment);
-		return new ResponseEntity<>(apartment, HttpStatus.OK);
-	}
-
-	@GetMapping("apartment/find/{id}")
-	@ApiOperation(value = "Encontra um apartamento através do id")
-	public ResponseEntity<?> getApartmentById(@PathVariable("id") Long id) {
-		Apartment apartment = propertyService.getApartmentById(id);
-		return new ResponseEntity<>(apartment, HttpStatus.OK);
-	}
-
-	@PutMapping("/update/apartment")
-	@ApiOperation(value = "Atualiza as informações de um apartamento")
-	public ResponseEntity<?> updateApartment(@RequestBody Apartment apartment) {
-		Apartment updatedApartment = propertyService.updateApartment(apartment);
-		return new ResponseEntity<>(updatedApartment, HttpStatus.OK);
-	}
-
-	@PostMapping("/ground")
-	@ApiOperation(value = "Salva um terreno")
-	public ResponseEntity<?> saveGround(@RequestBody PropertyDTO groundDto) {
-		Ground ground = new Ground();
-		ground.setArea(groundDto.getArea());
-		ground.setName(groundDto.getName());
-		ground.setNeighborhood(groundDto.getNeighborhood());
-		ground.setCodAddress(groundDto.getCodAddress());
-		ground.setCity(groundDto.getCity());
-		ground.setDescription(groundDto.getDescription());
-		ground.setAdress(groundDto.getAdress());
-		ground.setState(groundDto.getState());
-		ground.setPrice(groundDto.getPrice());
-		ground.setNumber(groundDto.getNumber());
-		ground.setAmount(groundDto.getAmount());
-		propertyService.saveProperty(ground);
-		return new ResponseEntity<>(ground, HttpStatus.OK);
-	}
-
-	@GetMapping("ground/find/{id}")
-	@ApiOperation(value = "Encontra um terreno através do id")
-	public ResponseEntity<?> getGroundById(@PathVariable("id") Long id) {
-		Ground ground = propertyService.getGroundById(id);
-		return new ResponseEntity<>(ground, HttpStatus.OK);
-	}
-
-	@PutMapping("/update/ground")
-	@ApiOperation(value = "Atualiza as informações de um terreno")
-	public ResponseEntity<?> updateApartment(@RequestBody Ground ground) {
-		Ground updatedGround = propertyService.updateGround(ground);
-		return new ResponseEntity<>(updatedGround, HttpStatus.OK);
-	}
-
-	@DeleteMapping("/delete/{id}")
-	@ApiOperation(value = "Deleta uma propriedade através do id")
-	public void deleteProperty(@PathVariable("id") Long id) {
-		propertyService.deleteProperty(id);
-	}
+	// --------------------------------
+	// --- ALL PROPERTIES ENDPOINTS ---
+	// --------------------------------
 	
 	@GetMapping("/all")
 	@ApiOperation(value = "Retorna uma lista com todas as propriedades")
@@ -185,8 +53,21 @@ public class PropertyController {
 		List<Property> properties = propertyService.findAllProperties();
 		return new ResponseEntity<>(properties, HttpStatus.OK);
 	}
-
-	@PostMapping("property/upload/{id}")
+	
+	@GetMapping("/find/{id}")
+	@ApiOperation(value = "Encontra uma propriedade através do id")
+	public ResponseEntity<?> getPropertyById(@PathVariable("id") Long id) {
+		Property property = propertyService.getPropertyById(id);
+		return new ResponseEntity<>(property, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	@ApiOperation(value = "Deleta uma propriedade através do id")
+	public void deleteProperty(@PathVariable("id") Long id) {
+		propertyService.deleteProperty(id);
+	}
+	
+	@PostMapping("/upload/{id}")
 	@ApiOperation(value = "Sobe três imagens para a propriedade")
 	public ResponseEntity<?> uploadImage(
 		@PathVariable("id") Long id, 
@@ -213,6 +94,141 @@ public class PropertyController {
 		propertyService.updateProperty(property);
 
 		return new ResponseEntity<>(property, HttpStatus.OK);
+	}
+
+	// ----------------------------
+	// --- APARTMENTS ENDPOINTS ---
+	// ----------------------------
+	
+	@PostMapping("/apartment")
+	@ApiOperation(value = "Salva um apartamento")
+	public ResponseEntity<?> saveApartment(@RequestBody PropertyDTO apartmentDto) {
+		Apartment apartment = new Apartment();
+		apartment.setArea(apartmentDto.getArea());
+		apartment.setName(apartmentDto.getName());
+		apartment.setNeighborhood(apartmentDto.getNeighborhood());
+		apartment.setCodAddress(apartmentDto.getCodAddress());
+		apartment.setCity(apartmentDto.getCity());
+		apartment.setDescription(apartmentDto.getDescription());
+		apartment.setAdress(apartmentDto.getAdress());
+		apartment.setState(apartmentDto.getState());
+		apartment.setPrice(apartmentDto.getPrice());
+		apartment.setNumber(apartmentDto.getNumber());
+		apartment.setBlock(apartmentDto.getBlock());
+		apartment.setRooms(apartmentDto.getRooms());
+		apartment.setAmount(apartmentDto.getAmount());
+		propertyService.saveProperty(apartment);
+		return new ResponseEntity<>(apartment, HttpStatus.OK);
+	}
+	
+	@GetMapping("apartment/all")
+	@ApiOperation(value = "Retorna uma lista de apartamentos")
+	public ResponseEntity<?> getAllApartments() {
+		List<Property> properties = propertyService.findAllByDtype("Apartment");
+		return new ResponseEntity<>(properties, HttpStatus.OK);
+	}
+	
+	@GetMapping("apartment/find/{id}")
+	@ApiOperation(value = "Encontra um apartamento através do id")
+	public ResponseEntity<?> getApartmentById(@PathVariable("id") Long id) {
+		Apartment apartment = propertyService.getApartmentById(id);
+		return new ResponseEntity<>(apartment, HttpStatus.OK);
+	}
+	
+	@PutMapping("apartment/update")
+	@ApiOperation(value = "Atualiza as informações de um apartamento")
+	public ResponseEntity<?> updateApartment(@RequestBody Apartment apartment) {
+		Apartment updatedApartment = propertyService.updateApartment(apartment);
+		return new ResponseEntity<>(updatedApartment, HttpStatus.OK);
+	}
+
+	// ------------------------
+	// --- HOUSES ENDPOINTS ---
+	// ------------------------
+	
+	@PostMapping("/house")
+	@ApiOperation(value = "Salva uma casa ")
+	public ResponseEntity<?> saveHouse(@RequestBody PropertyDTO houseDto) {
+		House house = new House();
+		house.setArea(houseDto.getArea());
+		house.setName(houseDto.getName());
+		house.setNeighborhood(houseDto.getNeighborhood());
+		house.setCodAddress(houseDto.getCodAddress());
+		house.setCity(houseDto.getCity());
+		house.setDescription(houseDto.getDescription());
+		house.setAdress(houseDto.getAdress());
+		house.setState(houseDto.getState());
+		house.setPrice(houseDto.getPrice());
+		house.setNumber(houseDto.getNumber());
+		house.setRooms(houseDto.getRooms());
+		house.setAmount(houseDto.getAmount());
+		propertyService.saveProperty(house);
+		return new ResponseEntity<>(house, HttpStatus.OK);
+	}
+	
+	@GetMapping("house/all")
+	@ApiOperation(value = "Retorna uma lista de casas")
+	public ResponseEntity<?> getAllHouses() {
+		List<Property> properties = propertyService.findAllByDtype("House");
+		return new ResponseEntity<>(properties, HttpStatus.OK);
+	}
+	
+	@GetMapping("house/find/{id}")
+	@ApiOperation(value = "Encontra uma casa através do ID")
+	public ResponseEntity<?> getHouseById(@PathVariable("id") Long id) {
+		House house = propertyService.getHouseById(id);
+		return new ResponseEntity<>(house, HttpStatus.OK);
+	}
+	
+	@PutMapping("house/update")
+	@ApiOperation(value = "Atualiza as informações de uma casa")
+	public ResponseEntity<?> updateHouse(@RequestBody House house) {
+		House updatedHouse = propertyService.updateHouse(house);
+		return new ResponseEntity<>(updatedHouse, HttpStatus.OK);
+	}
+
+	// -------------------------
+	// --- GROUNDS ENDPOINTS ---
+	// -------------------------
+	
+	@PostMapping("/ground")
+	@ApiOperation(value = "Salva um terreno")
+	public ResponseEntity<?> saveGround(@RequestBody PropertyDTO groundDto) {
+		Ground ground = new Ground();
+		ground.setArea(groundDto.getArea());
+		ground.setName(groundDto.getName());
+		ground.setNeighborhood(groundDto.getNeighborhood());
+		ground.setCodAddress(groundDto.getCodAddress());
+		ground.setCity(groundDto.getCity());
+		ground.setDescription(groundDto.getDescription());
+		ground.setAdress(groundDto.getAdress());
+		ground.setState(groundDto.getState());
+		ground.setPrice(groundDto.getPrice());
+		ground.setNumber(groundDto.getNumber());
+		ground.setAmount(groundDto.getAmount());
+		propertyService.saveProperty(ground);
+		return new ResponseEntity<>(ground, HttpStatus.OK);
+	}
+	
+	@GetMapping("ground/all")
+	@ApiOperation(value = "Retorna uma lista de terrenos")
+	public ResponseEntity<?> getAllGrounds() {
+		List<Property> properties = propertyService.findAllByDtype("Ground");
+		return new ResponseEntity<>(properties, HttpStatus.OK);
+	}
+
+	@GetMapping("ground/find/{id}")
+	@ApiOperation(value = "Encontra um terreno através do id")
+	public ResponseEntity<?> getGroundById(@PathVariable("id") Long id) {
+		Ground ground = propertyService.getGroundById(id);
+		return new ResponseEntity<>(ground, HttpStatus.OK);
+	}
+
+	@PutMapping("ground/update")
+	@ApiOperation(value = "Atualiza as informações de um terreno")
+	public ResponseEntity<?> updateApartment(@RequestBody Ground ground) {
+		Ground updatedGround = propertyService.updateGround(ground);
+		return new ResponseEntity<>(updatedGround, HttpStatus.OK);
 	}
 
 }
