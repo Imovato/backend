1f5d77bc2911583b689cf574e2808dad33414553
Samuel <samuelmodestoes@gmail.com>
2021-04-25 18:30:28 -0300

    correção da controller e pom

--- rent/src/main/java/com/example/rent/controller/RentController.java
+++ rent/src/main/java/com/example/rent/controller/RentController.java
@@ -1,7 +1,11 @@
 package com.example.rent.controller;
 
-
-
+import com.example.rent.dto.RentDto;
+import com.example.rent.interfaces.services.IPropertyService;
+import com.example.rent.interfaces.services.IRentServices;
+import com.example.rent.interfaces.services.IUserService;
+import com.example.rent.model.Property;
+import com.example.rent.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,48 +16,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.rent.dto.RentDTO;
-import com.example.rent.interfaces.services.IRentService;
+
 import com.example.rent.model.Rent;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
 
-
 @RestController
 @RequestMapping("/rent")
 @Api(value = "MICROSERVICE Rent")
 public class RentController {
-	
-	private IRentService rentService;
-	
-	public RentController (IRentService service) {
-		this.rentService = service;
-	}
-	
-	@PostMapping("/save")
-	@ApiOperation(value = "Salva uma arrendamento/aluguel")
-	public void saveRent(@RequestBody RentDTO dto) {
-		Rent rent = new Rent();
-		rent.setData(dto.getData());
-		rent.setProperty(dto.getProperty());
-		rent.setValue(dto.getAmountValue());
-		rentService.saveRent(rent);
-	}
-	
-    @PutMapping("/update")
-    @ApiOperation(value = "atualiza um arrendamento/aluguel")
-    public ResponseEntity<?> updateRent(@RequestBody Rent rent) {
-    	Rent updateRent = rentService.updateRent(rent);
-		return new ResponseEntity<>(updateRent, HttpStatus.OK);
+
+    private IRentServices rentService;
+    private IPropertyService propertyService;
+    private IUserService userService;
+
+
+    public RentController(IRentServices service) {
+        this.rentService = service;
     }
 
-	@GetMapping("/find/{id}")
-	@ApiOperation(value = "pega um arrendamento/aluguel pelo id")
-	public ResponseEntity<?> getRentById (@PathVariable("id") Long id) {
-		Rent rentFinded = rentService.findRentById(id);
-		return new ResponseEntity<>(rentFinded, HttpStatus.OK);
+    @PostMapping("/save")
+    @ApiOperation(value = "Salva uma arrendamento/aluguel")
+    public void saveRent(@RequestBody RentDto dto) {
+        Property property = propertyService.findPropertyById(dto.getId());
+        User user = userService.finUserById(dto.getId());
+        Rent rent = new Rent();
+        rent.setProperty(property);
+        rent.setValue(property.getPrice());
+        rentService.saveRent(rent);
     }
+
+//    @PutMapping("/update")
+//    @ApiOperation(value = "atualiza um arrendamento/aluguel")
+//    public ResponseEntity<?> updateRent(@RequestBody Rent rent) {
+//        Rent updateRent = rentService.updateRent(rent);
+//        return new ResponseEntity<>(updateRent, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/find/{id}")
+//    @ApiOperation(value = "pega um arrendamento/aluguel pelo id")
+//    public ResponseEntity<?> getRentById(@PathVariable("id") Long id) {
+//        Rent rentFinded = rentService.findRentById(id);
+//        return new ResponseEntity<>(rentFinded, HttpStatus.OK);
+//    }
 
 }
