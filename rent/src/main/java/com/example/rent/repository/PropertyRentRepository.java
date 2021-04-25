bb13af76528b6a598261ace1fbdc07a8b616af3f
Samuel <samuelmodestoes@gmail.com>
2021-04-25 18:29:33 -0300

    implementação das repositorys para aluguel

--- rent/src/main/java/com/example/rent/repository/PropertyRentRepository.java
+++ rent/src/main/java/com/example/rent/repository/PropertyRentRepository.java
@@ -1,13 +1,10 @@
 package com.example.rent.repository;
 
 
+import com.example.payment.model.PropertyRent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.rent.model.Property;
-
-
 @Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-    Property findPropertyById(Long id);
+public interface PropertyRentRepository extends JpaRepository<PropertyRent, Long> {
 }
--- rent/src/main/java/com/example/rent/repository/UserRepository.java
+++ rent/src/main/java/com/example/rent/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.rent.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rent.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findUserById(Long id);
 }
