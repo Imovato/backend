437c12e6e02896d9707a67f3f656a0ae559405fb
João Paulo Merlugo <joaomerlugo.aluno@unipampa.edu.br>
2022-11-22 18:55:51 -0300

    commit testes unit ac

--- acquisition/src/main/java/com/example/acquisition/repository/AcquisitionRepository.java
+++ acquisition/src/main/java/com/example/acquisition/repository/AcquisitionRepository.java
@@ -1,5 +1,6 @@
 package com.example.acquisition.repository;
 
+import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
