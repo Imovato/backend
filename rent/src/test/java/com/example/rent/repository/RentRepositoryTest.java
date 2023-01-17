aa938c4fc81c49f785bf7bd887ab925096affb9d
matt-balda <mateusbalda89@gmail.com>
2023-01-17 17:58:37 -0300

    Tests units Repository of Rent

---
+++ rent/src/test/java/com/example/rent/repository/RentRepositoryTest.java
@@ -0,0 +1,74 @@
+package com.example.rent.repository;
+
+import com.example.rent.model.Rent;
+import lombok.extern.log4j.Log4j2;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@DataJpaTest
+@DisplayName("Tests for Rent Repository")
+@Log4j2
+class RentRepositoryTest {
+
+    @Autowired
+    private RentRepository rentRepository;
+
+    @Test
+    @DisplayName("Save persists rent when Successful")
+    void save_PersistRent_WhenSuccessful() {
+       Rent rentToBeSaved = createRent();
+       Rent rentSaved = this.rentRepository.save(rentToBeSaved);
+        Assertions.assertThat(rentSaved).isNotNull();
+        Assertions.assertThat(rentSaved.getId()).isNotNull();
+        Assertions.assertThat(rentSaved.getValue()).isEqualTo(rentToBeSaved.getValue());
+    }
+
+    @Test
+    @DisplayName("Save updates rent when Successful")
+    void save_UpdatesRent_WhenSuccessful(){
+        Rent rentToBeSaved = createRent();
+        Rent rentSaved = this.rentRepository.save(rentToBeSaved);
+        rentSaved.setValue(1498D);
+        Rent rentUpdated = this.rentRepository.save(rentSaved);
+
+        Assertions.assertThat(rentUpdated).isNotNull();
+        Assertions.assertThat(rentUpdated.getId()).isNotNull();
+        Assertions.assertThat(rentUpdated.getValue()).isEqualTo(rentSaved.getValue());
+    }
+
+    @Test
+    @DisplayName("Delete removes rent when Successful")
+    void delete_RemovesRent_WhenSuccessful(){
+        Rent rentToBeSaved = createRent();
+        Rent rentSaved = this.rentRepository.save(rentToBeSaved);
+
+        this.rentRepository.delete(rentSaved);
+
+        Optional<Rent> rentOptional = this.rentRepository.findById(rentSaved.getId());
+        Assertions.assertThat(rentOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Find by rent when Successful")
+    void find_ByRent_WhenSuccessful(){
+        Rent rentToBeSaved = createRent();
+        Rent rentSaved = this.rentRepository.save(rentToBeSaved);
+        Rent rentFind = this.rentRepository.findById(rentSaved.getId()).get();
+
+        Assertions.assertThat(rentFind).isNotNull();
+        Assertions.assertThat(rentFind.getId()).isNotNull();
+    }
+
+    private Rent createRent() {
+        return Rent.builder()
+                .startDateRent(LocalDate.now())
+                .value(1250D)
+                .build();
+    }
+}
