11103316114c8b2a40ba73c1420a15c35ff395d9
matt-balda <mateusbalda89@gmail.com>
2023-02-12 15:55:05 -0300

    resolving conflits lol

--- rent/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tb_property (id, amount, price, status) VALUES (1, 2, 100, 'AVAILABLE');
-INSERT INTO tb_property (id, amount, price, status) VALUES (2, 3, 12300,'AVAILABLE');
-INSERT INTO tb_property (id, amount, price, status) VALUES (3, 6, 12300,'AVAILABLE');
-INSERT INTO tb_property (id, amount, price, status) VALUES (4, 5, 14300,'AVAILABLE');
-
-INSERT INTO tb_customer (id, name, cpf, salary, guarantor) VALUES (1, 'Joao','428.811.640-64','1300',1);
-INSERT INTO tb_customer (id, name, cpf, salary, guarantor) VALUES (2, 'Maria','343.562.430-21','1200',1);
-INSERT INTO tb_customer (id, name, cpf, salary, guarantor) VALUES (3, 'Anderson','123.123.123-66','1300',1);
-INSERT INTO tb_customer (id, name, cpf, salary, guarantor) VALUES (4, 'Andre','669.288.960-00','1400',0);
