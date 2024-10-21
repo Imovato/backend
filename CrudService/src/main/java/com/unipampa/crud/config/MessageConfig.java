be672968641386ec19715a7ccc0d044e4c4ec583
Samuel Modesto <samuelmodestoes@gmail.com>
2024-10-21 20:26:18 -0300

    implementando configurações para novas filas

--- CrudService/src/main/java/com/unipampa/crud/config/MessageConfig.java
+++ CrudService/src/main/java/com/unipampa/crud/config/MessageConfig.java
@@ -3,6 +3,7 @@ package com.unipampa.crud.config;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,25 +11,46 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MessageConfig {
 
-	@Value("${crud.rabbitmq.crudQueue}")
-	private String queueCrud;
+	@Value("${crud.rabbitmq.exchanges.exchangeAccommodations}")
+	private String exchangeAccommodations;
 
-	@Value("${crud.rabbitmq.exchange}")
-	private String exchange;
+	@Value("${crud.rabbitmq.exchanges.exchangeUsers}")
+	private String exchangeUsers;
+
+	@Value("${crud.rabbitmq.queues.accommodationQueue}")
+	private String accommodationQueue;
+
+	@Value("${crud.rabbitmq.queues.userQueue}")
+	private String userQueue;
 
 	@Bean
-	public Exchange declareExchange() {
-		return ExchangeBuilder.fanoutExchange(exchange).durable(true).build();
+	public Exchange exchangeAccommodations() {
+		return ExchangeBuilder.fanoutExchange(exchangeAccommodations).durable(true).build();
 	}
 
 	@Bean
-	public Queue queueOne() {
-		return new Queue(queueCrud, true);
+	public Exchange exchangeUsers() {
+		return ExchangeBuilder.fanoutExchange(exchangeUsers).durable(true).build();
 	}
 
 	@Bean
-	public Binding bindingQueueOne(FanoutExchange fanoutExchange, Queue queueOne) {
-		return BindingBuilder.bind(queueOne).to(fanoutExchange);
+	public Queue propertyQueue() {
+		return new Queue(accommodationQueue, true);
+	}
+
+	@Bean
+	public Queue userQueue() {
+		return new Queue(userQueue, true);
+	}
+
+	@Bean
+	public Binding bindingAccomodationQueue(@Qualifier("exchangeAccommodations") FanoutExchange fanoutExchange, Queue propertyQueue) {
+		return BindingBuilder.bind(propertyQueue).to(fanoutExchange);
+	}
+
+	@Bean
+	public Binding bindingUserQueue(@Qualifier("exchangeUsers") FanoutExchange fanoutExchange, Queue userQueue) {
+		return BindingBuilder.bind(userQueue).to(fanoutExchange);
 	}
 
 	@Bean
--- CrudService/src/main/java/com/unipampa/crud/sender/UserSender.java
+++ CrudService/src/main/java/com/unipampa/crud/sender/UserSender.java
@@ -2,6 +2,7 @@ package com.unipampa.crud.sender;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.unipampa.crud.model.User;
@@ -9,6 +10,7 @@ import com.unipampa.crud.model.User;
 @Component
 public class UserSender {
 
+	@Value("${crud.rabbitmq.exchanges.exchangeUsers}")
 	String exchange;
 
 	public RabbitTemplate rabbitTemplate;
