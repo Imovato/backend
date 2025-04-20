4974d949952071764375338b16179b8631b5478a
Auri Gabriel <ririmello.a@gmail.com>
2025-04-20 19:46:45 -0300

    docs(swagger): add schema to dtos and enums

--- cadastral/src/main/java/com/unipampa/crud/dto/AccommodationDTO.java
+++ cadastral/src/main/java/com/unipampa/crud/dto/AccommodationDTO.java
@@ -1,24 +1,61 @@
 package com.unipampa.crud.dto;
 
 import com.unipampa.crud.enums.AccommodationType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public record AccommodationDTO(
+		@Schema(example = "6804eaef24e9aa24141421a1")
 		String id,
-		@NotBlank String title,
+
+		@NotBlank
+		@Schema(example = "Apartamento moderno no centro")
+		String title,
+
+		@Schema(example = "Centro Histórico")
 		String neighborhood,
+
+		@Schema(example = "1234XYZ")
 		String codAddress,
-		@NotNull String city,
-		@NotBlank String description,
+
+		@NotNull
+		@Schema(example = "Porto Alegre")
+		String city,
+
+		@NotBlank
+		@Schema(example = "Apartamento bem iluminado, com 2 quartos e cozinha equipada.")
+		String description,
+
+		@Schema(example = "Rua dos Andradas")
 		String address,
-		@NotBlank String state,
-		@NotNull BigDecimal price,
+
+		@NotBlank
+		@Schema(example = "RS")
+		String state,
+
+		@NotNull
+		@Schema(example = "2500.00")
+		BigDecimal price,
+
+		@Schema(example = "123")
 		int streetNumber,
-		@NotNull Integer imageQuantity,
-		@NotNull AccommodationType accommodationType,
-		@NotNull Integer maxOccupancy,
+
+		@NotNull
+		@Schema(example = "2")
+		Integer imageQuantity,
+
+		@NotNull
+		@Schema(example = "APARTMENT")
+		AccommodationType accommodationType,
+
+		@NotNull
+		@Schema(example = "4")
+		Integer maxOccupancy,
+
+		@Schema(example = "[\"https://img.com/1.jpg\", \"https://img.com/2.jpg\"]")
 		List<String> imagesUrls
 ) {}
--- cadastral/src/main/java/com/unipampa/crud/enums/AccommodationType.java
+++ cadastral/src/main/java/com/unipampa/crud/enums/AccommodationType.java
@@ -2,11 +2,14 @@ package com.unipampa.crud.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public enum AccommodationType {
 
+    @Schema(description = "Apartamento")
     APARTMENT,
 
+    @Schema(description = "Casa")
     HOUSE;
 
 //    @JsonCreator
