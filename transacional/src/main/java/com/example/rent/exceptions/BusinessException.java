3dbb4f4e1393da7a95ff932445cdecccb0255c2e
Samuel Modesto <samuelmodestoes@gmail.com>
2026-02-03 18:02:07 -0300

    corrigindo a data inicial de entrada no imovel e adicionando o conceito de meses

---
+++ transacional/src/main/java/com/example/rent/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package com.example.rent.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
+
